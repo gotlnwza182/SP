9693cca0aae2fd65f27b3461d3c0b003aa42da86
STGot <74397231+gotlnwza182@users.noreply.github.com>
2021-12-13 17:19:26 +0700

    Update and rename login.html to login.php

--- login.php
+++ login.php
@@ -1,3 +1,10 @@
+<?php
+    session_start();
+    include('dbconnect.php'); 
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -20,11 +27,11 @@
     <!--Custom Css-->
     <link rel="stylesheet" href="./css/login-style.css" />
 
-    <title>Light Up | Login</title>
+    <title>Light Up | Home</title>
   </head>
   <body>
     <nav class="navbar navbar-dark bg-primary flex-md-nowrap p-0 shadow">
-      <a class="navbar-brand col-sm-3 col-md-2 head-icon" href="index.html">
+      <a class="navbar-brand col-sm-3 col-md-2 head-icon" href="index.php">
         <i class="bi bi-lightbulb"></i>
         <span>Light Up</span>
       </a>
@@ -42,15 +49,13 @@
       </button>
       <ul class="nav member-nav">
         <li class="nav-item member-nav-item">
-          <a class="nav-link member-nav-link login-mem-nav" href="login.html"
-            >Login</a
-          >
+          <a class="nav-link member-nav-link login-mem-nav" href="login.php">Login</a>
         </li>
         <li class="nav-item member-nav-item">
           <p class="nav-link member-nav-link">|</p>
         </li>
         <li class="nav-item member-nav-item">
-          <a class="nav-link member-nav-link signup-mem-nav" href="signup.html"
+          <a class="nav-link member-nav-link signup-mem-nav" href="signup.php"
             >Sign Up</a
           >
         </li>
@@ -62,43 +67,60 @@
         <main>
           <h2 class="login-head">LOGIN</h2>
           <hr />
-          <div class="fill-box">
-            <span class="title Username">Username</span>
-            <div class="col-4 col-offset-4 user-input">
-              <div class="input-group mb-3">
-                <input
-                  type="text"
-                  class="form-control"
-                  placeholder="Username"
-                  aria-label="Username"
-                  aria-describedby="basic-addon1"
-                />
+          <form action="login_db.php" method="post">
+            <div class="fill-box">
+              <span class="title Username">Username</span>
+              <div class="col-4 col-offset-4 user-input">
+                <div class="input-group mb-3">
+                  <input
+                    type="text" name="username"
+                    class="form-control"
+                    placeholder="Username"
+                    aria-label="Username"
+                    aria-describedby="basic-addon1"
+                  />
+                </div>
               </div>
-            </div>
-            <span class="title Password">Password</span>
-            <div class="col-4 col-offset-4 user-input">
-              <div class="input-group mb-3">
-                <input
-                  type="text"
-                  class="form-control"
-                  placeholder="6-20 Character"
-                  aria-label="6-20 Character"
-                  aria-describedby="basic-addon1"
-                />
+              <span class="title Password">Password</span>
+              <div class="col-4 col-offset-4 user-input">
+                <div class="input-group mb-3">
+                  <input
+                    type="password" name="password"
+                    class="form-control"
+                    placeholder="6-20 Character"
+                    aria-label="6-20 Character"
+                    aria-describedby="basic-addon1"
+                  />
+                </div>
               </div>
-            </div>
+              <?php if (isset($_SESSION['error'])) : ?>
+                <div style="width: 92%;
+                            margin: 0px auto;
+                            padding: 10px;
+                            border: 1px solid #a94442;
+                            color: #a94442;
+                            background: #f2dede;
+                            border-radius: 5px;
+                            text-align: center;">
+                  <h5>
+                    <?php 
+                        echo $_SESSION['error'];
+                        unset($_SESSION['error']);
+                    ?>
+                  </h5>
+                </div>
+              <?php endif ?>
 
-            <button type="button" class="btn btn-primary btn-lg btn-login">
-              LOGIN
-            </button>
-            <span class="gosignup"
-              >Don't have any account?<a
-                href="signup.html"
-                class="click-to-signup"
-                >Click here.</a
-              ></span
-            >
-          </div>
+              <button type="submit" name="login_user" class="btn btn-primary btn-lg btn-login">
+                LOGIN
+              </button>
+              <span class="gosignup"
+                >Don't have any account?<a href="signup.php" class="click-to-signup"
+                  >Click here.</a
+                ></span
+              >
+            </div>
+          </form>
         </main>
       </div>
     </div>
